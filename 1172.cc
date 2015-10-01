e772924350302f12173756948100ba517c2d2f89
TakeumiYamamura <yama1223xxx@gmail.com>
2015-10-01 22:46:34 +0900

    チェビシェフの定理

--- 1172.cc
+++ 1172.cc
@@ -2,34 +2,57 @@
 
 using namespace std;
 
-bool prime_check(int n){
-  prime_count = 0;
-  for (int i = 2; i < n; ++i)
+
+bool primes[246913];
+int max_num = 246912;
+void set_primes(){
+  primes[0] = false;
+  primes[1] = false;
+  int top_prime, count;
+  top_prime = 2;
+  for (int i = 2; i < max_num+1; ++i)
   {
-    if (n%i == 0){
-      prime_count ++;
-      if (prime_count > 1)
+    primes[i] = true;
+  }
+  while(true){
+    count = top_prime;
+    count += top_prime;
+    for (int i = 0; count < max_num+1; ++i)
+    {
+      primes[count] = false;
+      count += top_prime;
+    }
+    for (int i = top_prime+1; i < max_num+1; ++i)
+    {
+      if (primes[i])
       {
-        return false;
+        top_prime = i;
+        break;
       }
     }
-  }
-  if (prime_count == 1){
-    return true;
+    if (top_prime * 2 > max_num){
+      break;
+    }
   }
 }
 
+int primes_count(int n){
+  int count;
+  count = 0;
+  for (int i = n+1; i <= 2*n; ++i)
+  {
+    if (primes[i]){
+      count ++;
+    }
+  }
+  return count;
+}
 
 int main(){
   int n;
+  set_primes();
   while(cin >> n, n){
-    count = 0;
-    for (int i =  n+1; i <= 2*n ; ++i)
-    {
-      if (prime_check(i)){
-        count++ ;
-      }
-    }
-    cout << count << endl;
+    cout << primes_count(n) << endl;
   }
+  return 0;
 }
