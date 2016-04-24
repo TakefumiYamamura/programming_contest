7c574b6bf68f73abe3e73ff08072e53e70285ac8
takefumi yamamura <yama1223xxx@gmail.com>
2016-04-25 04:35:38 +0900

    rooted trees

---
+++ treee_walk.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node { int p, l, r; };
+struct Node Nodes[10000];
+
+int node_num
+
+int read_input(){
+	cin >> node_num;
+	for (int i = 0; i < node_num; ++i)
+	{
+		cin >> Nodes[i].
+	}
+}
+
+int main(){
+
+}
