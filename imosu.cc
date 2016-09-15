9d8543589ad6fdefd52080cfe56cea56cc6d1ed3
TakefumiYamamura <yama1223xxx@gmail.com>
2016-09-15 23:10:06 +0800

    imosu

---
+++ imosu.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, q, l, r;
+	cin >> n >> q;
+	vector<int> osero;
+	osero.resize(n, 0)
+	for (int i = 0; i < q; ++i)
+	{
+		cin >> l >> r;
+		osero[l-1]++;
+		osero[r-1]--;
+	}
+	for (int i = 1; i < n; ++i)
+	{
+		osero[i] += osero[i-1];
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		cout << osero[i]
+	}
+	cout << endl;
+}
