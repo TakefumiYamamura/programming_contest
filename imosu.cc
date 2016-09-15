6ddd6405c70a4bb8431259f320c539100f8295a7
TakefumiYamamura <yama1223xxx@gmail.com>
2016-09-15 23:20:29 +0800

    fix imosu.cc

--- imosu.cc
+++ imosu.cc
@@ -6,21 +6,20 @@ int main(){
 	int n, q, l, r;
 	cin >> n >> q;
 	vector<int> osero;
-	osero.resize(n, 0)
+	osero.resize(n+1, 0);
 	for (int i = 0; i < q; ++i)
 	{
 		cin >> l >> r;
 		osero[l-1]++;
-		osero[r-1]--;
+		osero[r]--;
 	}
 	for (int i = 1; i < n; ++i)
 	{
 		osero[i] += osero[i-1];
 	}
-
 	for (int i = 0; i < n; ++i)
 	{
-		cout << osero[i]
+		cout << osero[i] % 2;
 	}
 	cout << endl;
 }
