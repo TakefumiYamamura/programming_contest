b705f0f42a17e32b2be919563cde57d8bf075d99
TakefumiYamamura <yama1223xxx@gmail.com>
2016-10-03 20:09:36 +0900

    divide a cuboid

---
+++ agc4-a.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+long long min(long long a, long long b){
+	if(a > b){
+		return b;
+	}else{
+		return a;
+	}
+}
+
+
+int main(){
+	long long a, b, c;
+	cin >> a >> b >> c ;
+	if(a%2 == 0 || b%2 == 0 || c%2 == 0){
+		cout << 0 << endl;
+		return 0;
+	}
+
+	long long ans = min(a*b, min(b*c, c*a));
+	cout << ans << endl;
+
+}
