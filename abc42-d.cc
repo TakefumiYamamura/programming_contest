bf9f4dbca0987e9c99b23c1037bc2a2a2ff15287
TakefumiYamamura <yama1223xxx@gmail.com>
2016-10-17 00:54:26 +0900

    逆元

--- abc42-d.cc
+++ abc42-d.cc
@@ -2,35 +2,59 @@
 #define ll long long 
 using namespace std;
 
-const ll MAX = 1e9+7;
+const ll MOD = 1e9+7;
 
 ll factorial[200003];
+ll revfac[200003];
+// ll power[200003] = {0};
 
 ll con(ll a, ll b){
-	return factorial[a]/(factorial[b]*factorial[a-b] % MAX) % MAX; 
+	return factorial[a] * revfac[b] % MOD * revfac[a-b] % MOD; 
 }
+
+ll pow(ll a, ll e){
+	ll tmp = 1;
+	while(e > 0){
+		if( e%2 != 0){
+			tmp = tmp * a % MOD ;
+		}else{
+			tmp = tmp * tmp % MOD;
+		}
+		e /= 2; 
+	}
+	return tmp % MOD;
+	// if(e == 0){
+	// 	return 1;
+	// }
+	// if(e%2 == 0){
+	// 	return pow(a, e/2) * pow(a, e/2) % MOD;
+	// }else{
+	// 	return pow(a, e-1) * a % MOD;
+	// }
+}
+
 
 void set_factorial(ll n){
 	factorial[0] = 1;
+	revfac[0] = 1; 
 	for (int i = 1; i <= n; ++i)
 	{
-		factorial[i] = factorial[i-1] * i % MAX;
-		factorial[i] %= MAX;
-
+		factorial[i] = factorial[i-1] * i % MOD;
+		revfac[i] = pow(factorial[i], MOD - 2) % MOD;
 	}
 }
 
 int main(){
 	ll h, w, a, b;
 	cin >> h >> w >> a >> b;
+
 	set_factorial(h+w);
 	ll ans = 0;
 	for (int i = 0; i <= h-a-1; ++i)
 	{
-		ans += con(b+i-1, i) * con(w-b+h-i-2, w-b-1) % MAX;
-		// cout << ans << endl;
-		ans = ans%MAX;
+		ans += con(b+i-1, i) * con(w-b+h-i-2, w-b-1) % MOD;
+		cout << con(b+i-1, i) <<  " " << con(w-b+h-i-2, w-b-1) << endl;
 	}
-	cout << ans % MAX << endl;
+	cout << ans % MOD << endl;
 
 }
