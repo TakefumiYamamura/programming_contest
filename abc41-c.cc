8363372e62299dd5ec1b9a9ad29f89a6eaf1730e
TakefumiYamamura <yama1223xxx@gmail.com>
2016-10-17 15:09:11 +0900

    delete comments

--- abc41-c.cc
+++ abc41-c.cc
@@ -6,38 +6,6 @@
 #define MAX 1000001
 using namespace std;
 
-// int n;
-// int a[MAX], b[1000000001];
-
-// void merge_sort(int start, int finish){
-// 	int mid = finish/2;
-// 	int right[MAX];
-// 	int left[MAX];
-// 	if(start - mid == 0){
-// 		return ;
-// 	}
-//     merge_sort(start, finish/2);
-// 	merge_sort(finish/2 + 1, finish);
-// 	for (int i = 1; i <= mid; ++i)
-// 	{
-// 		right[i] = a[i];
-// 	}
-// 	for (int i = 1; i <= finish - mid; ++i)
-// 	{
-// 		left[i] = a[mid + i];
-// 	}
-// 	int r = 1;
-// 	int l = 1;
-// 	for (int i = 1; i <= finish - start; ++i)
-// 	{
-// 		if(right[r] > left[l]){
-// 			a[start+i] = right[r];
-// 			r++;
-// 		}else{
-// 			a[start+i] = left[l];
-// 		}
-// 	}
-// }
 
 struct Num{
 	ll value, index;
@@ -51,15 +19,12 @@ int main(){
 	vector<Num> nums;
 	for (int i = 1; i <= n; ++i)
 	{
-		// cin >> a[i];
-		// b[a[i]] = i;	
 		Num num;
 		cin >> num.value;
 		num.index = i;
 		nums.push_back(num);
 	}
 	sort(nums.begin(), nums.end(), Num::Asc);
-	// merge_sort(1,n);
 	for (int i = 0; i < n; ++i)
 	{
 		cout << nums[i].index << endl;
