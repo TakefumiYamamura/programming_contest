a70ea491b63455a929e99760c488056d5aa851fe
TakefumiYamamura <yama1223xxx@gmail.com>
2016-10-31 18:33:25 +0900

    programming lecture

---
+++ at_coder/abc3-c.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int n, k;
+	cin >> n >> k;
+	vector<double> nums;
+	double ans = 0.0000000;
+
+	for (int i = 0; i < n; ++i)
+	{
+		double tmp;
+		cin >> tmp;
+		nums.push_back(tmp);
+	}
+
+	std::sort(nums.begin(), nums.end());
+
+	for (int i = n-k; i < n; ++i)
+	{
+		ans = (ans + nums[i]) / 2.0000000;
+	}
+
+	// cout << ans << endl;
+	printf("%.6f\n", ans );
+
+}
