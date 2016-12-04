aae99e0aef6ba6f40c4585992cc5c37d7eb688d4
TakefumiYamamura <yama1223xxx@gmail.com>
2016-12-04 23:14:32 +0900

    greedy

--- at_coder/arc64-c.cc
+++ at_coder/arc64-c.cc
@@ -2,25 +2,60 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-class arc64
+
+class BoxesAndCandies
 {
 public:
-	arc64();
-	~arc64();
+	long long n, x;
+	long long sum;
+	// int dp[100001][2];
+	vector<long long> a;
+	BoxesAndCandies();
+	~BoxesAndCandies();
 	void exec();
 };
 
-arc64::arc64(){
-
+BoxesAndCandies::BoxesAndCandies(){
+	cin >> n >> x;
+	a.push_back(0);
+	for (long long i = 0; i < n; ++i)
+	{
+		long long tmp;
+		cin >> tmp;
+		a.push_back(tmp);
+	}
+	a.push_back(0);
 }
 
-arc64::~arc64(){}
+BoxesAndCandies::~BoxesAndCandies(){}
 
-void arc64::exec(){}
+void BoxesAndCandies::exec(){
+	sum = 0;
+	vector<long long> b = a;
+	for (int i = 1; i <= n; ++i)
+	{
+		if(a[i-1] + a[i] > x){
+			sum += a[i] + a[i-1] - x;
+			a[i] -= a[i] + a[i-1] - x;
+		}
+	}
+	long long r_sum = 0;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if(b[i+1] + b[i] > x){
+			r_sum += b[i] + b[i+1] - x;
+			b[i] -= b[i] + b[i+1] - x;
+		}
+	}
+	cout << min(sum, r_sum) << endl;
+}
 
 int main(){
-	
+	BoxesAndCandies bac = BoxesAndCandies();
+	bac.exec();
 }
