8b52cea896dd0c525dc54f3043acf56323a92a0c
TakefumiYamamura <yama1223xxx@gmail.com>
2016-12-14 18:58:13 +0900

    PalindromeInstallments

---
+++ at_coder/arc53-b.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class PalindromeInstallments
+{
+public:
+	string str;
+	PalindromeInstallments(){
+		cin >> str;
+	}
+	void exec();
+};
+
+void PalindromeInstallments::exec(){
+	int bit = 0;
+	for (int i = 0; i < str.length(); ++i)
+	{
+		if(bit >> (str[i] - 'a') & 1){
+			bit = bit & ~(1 << (str[i] - 'a'));
+		}else{
+			bit |= 1 << (str[i] - 'a'); 
+		}
+	}
+	int odd = 0;
+	for (int i = 0; i < 26; ++i)
+	{
+		if(bit >> i & 1){
+			odd++;
+		}
+	}
+
+	if(odd <= 1){
+		cout << str.length() << endl;
+		return;
+	}
+	if(odd % 2 == 1){
+		cout << str.length() / odd << endl;
+	}else{
+		cout << str.length() / odd - 1 << endl;
+	}
+}
+
+int main(){
+	PalindromeInstallments pi = PalindromeInstallments();
+	pi.exec();
+}
