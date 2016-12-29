29905e745d248e42efe97adcf7748c4f1319e114
TakefumiYamamura <yama1223xxx@gmail.com>
2016-12-29 20:01:01 +0900

    segTree

--- at_coder/segTree.cc
+++ at_coder/segTree.cc
@@ -19,7 +19,7 @@ public:
 	int query(int a, int b, int k, int l, int r);
 };
 
-SegmentTree::SegmentTree(int n){
+SegmentTree::SegmentTree(int n_){
 	n = 1;
 	while(n < n_){
 		n *= 2;
@@ -31,21 +31,37 @@ SegmentTree::SegmentTree(int n){
 SegmentTree::~SegmentTree(){}
 
 void SegmentTree::update(int k, int a){
-	k += n-1;
+	k += n - 1;
 	dat[k] = a;
 	while(k > 0){
-		k = (k-1)/2
-		dat[k] = min(dat[k*2 +1], dat[k*2+2]);
+		k = (k - 1) / 2;
+		dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
 	}
 }
 
 //区間a-bの最小値を求める。
 //後ろのほうの引数は、計算の簡単のための引数。
+//外からはquery(a,b,0,0,n)のように呼ぶ
 int SegmentTree::query(int a, int b, int k, int l, int r){
-	
+	if(r <= a || b <= l) return INT_MAX;
+	if(a <= l && r <= b){
+		return dat[k];
+	}else{
+		int vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+		int vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+		return min(vl, vr);
+	}
 }
 
 
 int main(){
+	int a[10] = {10,9,8,7,5,4,7,8,9,10};
 
+	SegmentTree st = SegmentTree(10);
+	for (int i = 0; i < 10; ++i)
+	 {
+	 	st.update(i, a[i]);
+	 }
+	 //(3,5] 8,7 => 7
+	 cout << st.query(3, 5, 0, 0, 10) << endl;
 }
