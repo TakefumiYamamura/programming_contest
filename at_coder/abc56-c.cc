d5a9363b8e1c479252e0e88f9e5763bcba7509ed
TakefumiYamamura <yama1223xxx@gmail.com>
2017-03-29 10:01:25 +0900

    abc56-c.cc

---
+++ at_coder/abc56-c.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class GoHome
+{
+private:
+	int x;
+public:
+	GoHome(){
+		cin >> x;
+	}
+
+	void exec(){
+		int ans = 1;
+		int sum = 0;
+		while(sum < x){
+			sum += ans;
+			ans++;
+		}
+		cout << ans - 1 << endl;
+	}
+
+};
+
+int main(){
+	GoHome gh = GoHome();
+	gh.exec();
+
+}
