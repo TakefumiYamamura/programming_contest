bd399c7f5a09c3386f8b57047d6530980ef959a6
TakefumiYamamura <yama1223xxx@gmail.com>
2017-07-29 22:48:22 +0900

    arc77-d.cc

---
+++ at_coder/arc77-d.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+#define ll long long
+#define ull unsigned long long
+#define eps 10e-10
+#define MOD 1000000007
+
+using namespace std;
+
+class Conbination
+{
+public:
+	int size;
+	vector<ll> fac;
+	vector<ll> revfac;
+	Conbination(int n);
+	~Conbination();
+	void setFac();
+	ll nCk(ll n, ll k);
+	ll pow(ll a, ll e);
+};
+
+Conbination::Conbination(int n){
+	this->size = n;
+	fac.reserve(n+1);
+	revfac.reserve(n+1);
+	setFac();
+}
+
+Conbination::~Conbination(){}
+
+void Conbination::setFac(){
+	fac[0] = 1;
+	revfac[0] = 1;
+	for (int i = 1; i <= size; ++i)
+	{
+		fac[i] = i * fac[i-1] % MOD;
+		revfac[i] = pow(fac[i], MOD - 2) % MOD;
+	}
+}
+
+ll Conbination::pow(ll a, ll e){
+	ll ans = 1;
+	while(e > 0){
+		if(e & 1){
+			ans *= a;
+			ans %= MOD;
+		}
+		a *= a;
+		a %= MOD;
+		e = e >> 1;
+	}
+	return ans % MOD;
+}
+
+ll Conbination::nCk(ll n, ll k){
+	if(n < k) return 0;
+	return fac[n] * revfac[k] % MOD * revfac[n - k] % MOD;
+}
+
+int main() {
+	ll n;
+	cin >> n;
+	ll a;
+	ll b;
+	unordered_map<ll, ll> um;
+	for (ll i = 0; i < n+1; ++i)
+	{
+		ll tmp;
+		cin >> tmp;
+		if(um.find(tmp) == um.end()) {
+			um[tmp] = i;
+		} else{
+			a = um[tmp];
+			b = n - i;
+		}
+	}
+
+ 	Conbination c = Conbination(n+1);
+ 	for (ll i = 1; i <= n+1; ++i)
+ 	{
+ 		cout << c.nCk(n+1, i) - c.nCk(a + b, i-1) << endl;
+ 	}
+}
